5bf2f3b8f2cdebe14c9c58ac8deb6f5d00bbde0f
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-11-09 00:08:26 +0530

    new program

--- src/Java_Program_From_PAVAN/CountDigitInNumber.java
+++ src/Java_Program_From_PAVAN/CountDigitInNumber.java
@@ -3,7 +3,16 @@ package Java_Program_From_PAVAN;
 public class CountDigitInNumber {
 	public static void main(String[] args) {
 		
-		
+		int num=123456;
+		int count=0;
+		 
+		while(num>0)
+		{
+			num=num/10;
+			count++;
+			
+		}
+		System.out.println(count);
 	}
 
 }
