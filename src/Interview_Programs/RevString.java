6c7f8d641f8eb40d88484347e46510d0d08f1165
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-10 11:47:48 +0530

    new added

--- src/Interview_Programs/RevString.java
+++ src/Interview_Programs/RevString.java
@@ -3,6 +3,16 @@ package Interview_Programs;
 public class RevString {
 	
 	public static void main(String[] args) {
+		String str="Hello Java";
+		String rev="";
+		String sp[]=str.split(" ");
+		for(int i=sp.length-1;i>=0;i--)
+		{
+			rev=rev+" "+sp[i];
+			
+		}
+		System.out.println(rev);
+		
 		
 	}
 
