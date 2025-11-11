8e4e68464e3845e1e81492a0148ea5bab25014d5
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2025-11-11 18:15:22 +0530

    new program added

--- src/StringDemo/CharDemo.java
+++ src/StringDemo/CharDemo.java
@@ -6,15 +6,22 @@ import java.util.Map;
 public class CharDemo {
 
 	public static void main(String[] args) {
-		
-		String str="Hello java";
-		String sp[]=str.split(" ");
-		
-		Map<Charactor,Integer>map=new HashMap<>();
-		
-		for(int i=0;i<sp.length;i++)
-		{
+
+		String str = "Hello java";
+		Map<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < str.length(); i++) {
 			
+			char ch = str.charAt(i);
+
+			if (map.containsKey(ch)) {
+				int count = map.get(ch);
+				map.put(ch, count + 1);
+
+			} else {
+				map.put(ch, 1);
+			}
 		}
+		System.out.println(map);
 	}
 }
