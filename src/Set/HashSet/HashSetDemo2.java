cf4ebcc8875609dbd5a9f78349359fef362ad5fc
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2025-11-29 07:17:13 +0530

    new

--- src/Set/HashSet/HashSetDemo2.java
+++ src/Set/HashSet/HashSetDemo2.java
@@ -5,20 +5,19 @@ import java.util.HashSet;
 public class HashSetDemo2 {
 
 	public static void main(String[] args) {
-		HashSet<Integer> hs=new HashSet<>();
+		HashSet<Integer> hs = new HashSet<>();
 		hs.add(2);
 		hs.add(4);
 		hs.add(6);
-		System.out.println("HashSet :"+hs);
-		
-		HashSet<Integer>num=new HashSet<>();
+		System.out.println("HashSet :" + hs);
+
+		HashSet<Integer> num = new HashSet<>();
 		num.addAll(hs);
 		num.add(8);
-		
-		
-		System.out.println("New hashSet :"+num);
-		
-		//remove all 
+
+		System.out.println("New hashSet :" + num);
+
+		// remove all
 		num.removeAll(hs);
 		System.out.println(num);
 
