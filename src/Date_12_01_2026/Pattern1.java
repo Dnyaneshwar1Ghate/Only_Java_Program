192f5474d86f079fe4fa16ef83edc1e2f82ae087
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-01-12 19:00:45 +0530

    new

---
+++ src/Date_12_01_2026/Pattern1.java
@@ -0,0 +1,17 @@
+package Date_12_01_2026;
+
+public class Pattern1 {
+	
+	public static void main(String[] args) {
+		
+		//input string ="java";
+		//output=j#v##a###v####;
+		
+		String input="java";
+		int count=0;
+		
+		
+		
+	}
+
+}
