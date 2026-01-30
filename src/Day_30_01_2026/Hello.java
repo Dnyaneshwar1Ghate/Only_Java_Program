c9f50eda38870330f3c4baf9f75562df4bc824da
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-01-30 18:51:26 +0530

    new program

---
+++ src/Day_30_01_2026/Hello.java
@@ -0,0 +1,9 @@
+package Day_30_01_2026;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("HEllo");
+	}
+
+}
---
+++ src/Day_30_01_2026/VariablesExample.java
@@ -0,0 +1,10 @@
+package Day_30_01_2026;
+
+public class VariablesExample {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
