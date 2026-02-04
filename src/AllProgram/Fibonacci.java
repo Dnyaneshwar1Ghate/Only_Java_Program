1ea695ff4b614531ea474804030afdd3735710fe
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-04 14:33:37 +0530

    new

---
+++ src/AllProgram/Fibonacci.java
@@ -0,0 +1,16 @@
+package AllProgram;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+		
+		int num=50;
+		int num1=0;
+		int num2=1;
+		
+		while(num>0)
+		{
+			System.out.println(num1+" "+num2);
+		}
+	}
+
+}
