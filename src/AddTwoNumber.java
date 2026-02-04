22c09b1e2bd0acc150dddcb7c499cb1bb7ccf03e
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-04 12:56:19 +0530

    new

---
+++ src/AddTwoNumber.java
@@ -0,0 +1,19 @@
+
+public class AddTwoNumber {
+	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(add(50,60));
+	}
+	
+	public static int add(int a,int b)
+	
+	{
+		return a+b;
+		
+	}
+	
+
+}
