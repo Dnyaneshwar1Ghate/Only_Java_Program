ae6549800a048a3087b9ae6cbdf66ac56fff42e2
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-12 18:45:21 +0530

    new

---
+++ src/Day_12_02_2026/IntegerDemo.java
@@ -0,0 +1,13 @@
+package Day_12_02_2026;
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		
+		int num=50600;
+		int x=(10,50);
+		System.out.println(num);
+		System.out.println(x);
+		
+	}
+}
