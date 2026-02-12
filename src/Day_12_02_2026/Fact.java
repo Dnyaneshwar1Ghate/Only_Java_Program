5bd2f6339298b24b76f525f4ee7270b6beb397ec
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-12 11:48:44 +0530

    new

---
+++ src/Day_12_02_2026/Fact.java
@@ -0,0 +1,16 @@
+package Day_12_02_2026;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		
+		int num=5;
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	
+	}
+}
---
+++ src/Day_12_02_2026/Fibonacci.java
@@ -0,0 +1,20 @@
+package Day_12_02_2026;
+
+public class Fibonacci {
+
+public static void main(String[] args) {
+	
+	int num=5;
+	int num1=0;
+	int num2=1;
+	System.out.print(num1+" "+num2);
+	for(int i=2;i<=num;i++)
+	{
+		int num3=num1+num2;
+		System.out.print(" "+num3);
+		num1=num2;
+		num2=num3;
+		
+	}
+}
+}
