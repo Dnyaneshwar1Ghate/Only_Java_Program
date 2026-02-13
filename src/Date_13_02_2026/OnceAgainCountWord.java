8adb3535d8da87227d53d4cdad0c76c6ef66d01e
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-13 18:52:39 +0530

    new

--- src/Date_13_02_2026/OnceAgainCountWord.java
+++ src/Date_13_02_2026/OnceAgainCountWord.java
@@ -3,27 +3,27 @@ package Date_13_02_2026;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CountWordFromString {
-	
+public class OnceAgainCountWord {
+
 	public static void main(String[] args) {
-		String str="new new new java java";
+		String str="hello hello this this is is java java programming";
 		String sp[]=str.split(" ");
 		Map<String,Integer>map=new HashMap<>();
 		for(int i=0;i<sp.length;i++)
 		{
 			if(!map.containsKey(sp[i]))
 			{
-				
 				map.put(sp[i], 1);
-				
-			}else {
-				
-				int count =map.get(sp[i]);
-				map.put(sp[i], count+1);
 			}
+			else{
+				
+				int count=map.get(sp[i]);
+				map.put(sp[i],count+1);
+			}
+			
+			
 		}
 		System.out.println(map);
-				
+		
 	}
-
 }
