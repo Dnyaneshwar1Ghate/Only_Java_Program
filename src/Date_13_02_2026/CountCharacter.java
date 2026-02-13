53f81c7ecc2175de8849e8041e3db189360e6801
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-13 18:58:31 +0530

    new

---
+++ src/Date_13_02_2026/CountCharacter.java
@@ -0,0 +1,27 @@
+package Date_13_02_2026;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCharacter {
+
+	public static void main(String[] args) {
+		
+		String str="automation";
+		char[]ch=str.toCharArray();
+		Map<Character,Integer>map=new HashMap<>();
+		
+		for(int i=0;i<str.length();i++)
+		{
+			if(map.containsKey(ch[i]))
+			{
+				int count=map.get(ch[i]);
+				map.put(ch[i], count+1);
+			}else {
+				map.put(ch[i], 1);
+			}
+		}
+		System.out.println(map);
+		
+	}
+}
