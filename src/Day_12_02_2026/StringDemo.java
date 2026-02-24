7088f80a1c17b9ad857277dc835f8eb742882dea
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-02-24 18:30:30 +0530

    new program

---
+++ src/Day_12_02_2026/StringDemo.java
@@ -0,0 +1,13 @@
+package Day_12_02_2026;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+
+		String st1="Hello";
+		StringBuffer db=new StringBuffer("ghate");
+		String newString=new StringBuffer("Hello").toString();
+		System.out.println(st1+" "+db);
+		
+	}
+}
