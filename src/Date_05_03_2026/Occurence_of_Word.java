4fea179ab925fa9ea0a02aa54c100a3728191d4e
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-03-06 12:37:08 +0530

    new

---
+++ src/Date_05_03_2026/Occurence_of_Word.java
@@ -0,0 +1,15 @@
+package Date_05_03_2026;
+
+public class Occurence_of_Word {
+	
+	static void occWords(String str)
+	{
+		String [] sp=str.split(" ");
+		
+		
+	}
+	public static void main(String[] args) {
+	
+		occWords("hello this is is is java java hello hello");
+	}
+}
