675c41f279e4866f54aaed7de4eb38fc1fc90e37
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-03-06 13:51:29 +0530

    new

---
+++ src/Date_05_03_2026/LengthOSrray.java
@@ -0,0 +1,16 @@
+package Date_05_03_2026;
+
+
+public class LengthOSrray {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {10,20,65,04,50,12,51,6};
+		int count=0;
+		for(int a:arr)
+		{
+			count++;
+		}
+		System.out.println(count);
+	}
+}
--- src/Date_05_03_2026/Occurence_of_Word.java
+++ src/Date_05_03_2026/Occurence_of_Word.java
@@ -5,7 +5,23 @@ public class Occurence_of_Word {
 	static void occWords(String str)
 	{
 		String [] sp=str.split(" ");
-		
+		for(int i=0;i<sp.length;i++)
+		{
+			int count=1;
+			for(int j=i+1;j<sp.length;j++)
+			{
+				if(sp[i].equals(sp[j]))
+				{
+					count++;
+					sp[j]="0";
+				}
+			}
+			if(!sp[i].equals("0"))
+			{
+				System.out.println(sp[i]+":"+count);
+			}
+			
+		}
 		
 	}
 	public static void main(String[] args) {
