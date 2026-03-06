ec0166e171094a6bc2a5a6fa62b9e871562c9029
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-03-06 10:58:29 +0530

    new

--- src/Date_05_03_2026/CharOccure.java
+++ src/Date_05_03_2026/CharOccure.java
@@ -5,10 +5,28 @@ public class CharOccure {
 	static void occurenceChar(String str)
 	{
 		
+		char[]ch=str.toCharArray();
+		for(int i=0;i<str.length();i++)
+		{
+			int count=1;
+			for(int j=i+1;j<str.length();j++)
+			{
+				if(ch[i]==ch[j])
+				{
+					count++;
+					ch[j]='0';
+				}
+			}
+			if(ch[i]!='0')
+			{
+				System.out.println(ch[i]+" "+count);
+				
+			}
+		}
 		
 	}
 	
 	public static void main(String[] args) {
-		occurenceChar("hello java");
+		occurenceChar("hellojava");
 	}
 }
