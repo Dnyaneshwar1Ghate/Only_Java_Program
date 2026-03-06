bc69772161ff3f8708339f52e2ebcf75b32dc6cf
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-03-06 12:25:12 +0530

    new

---
+++ src/Date_05_03_2026/OcOFCha.java
@@ -0,0 +1,32 @@
+package Date_05_03_2026;
+
+public class OcOFCha {
+
+	static void ocChar(String str)
+	{
+		char ch[]=str.toCharArray();
+		for(int i=0;i<ch.length;i++)
+		{
+			int count=1;
+			for(int j=i+1;j<ch.length;j++)
+			{
+				if(ch[i]==ch[j])
+				{
+					count++;
+					ch[j]='0';
+				}
+				
+			}
+			if(ch[i]!='0'&& ch[i]>1&&ch[i]!=' ')
+			{
+					System.err.println(ch[i]+" "+count);
+			}
+			
+		}
+		
+	}
+	public static void main(String[] args) {
+		
+		ocChar("Hello java");
+	}
+}
