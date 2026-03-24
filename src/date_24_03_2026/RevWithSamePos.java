08fc6b0195373b1b4951dd20aae03ace024165e8
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-03-24 17:04:54 +0530

    new

---
+++ src/date_24_03_2026/RevWithSamePos.java
@@ -0,0 +1,20 @@
+package date_24_03_2026;
+
+public class RevWithSamePos {
+
+	public static void main(String[] args) {
+		
+		String str="hello java";
+		String rev="";
+		String string[]=str.split(" ");
+		for(int i=0;i<string.length;i++)
+		{
+			String temp=string[i];
+			String inRev=" ";
+			for(int j=temp.length()-1;j>0;j--)
+			{
+				rev=rev+
+			}
+		}
+	}
+}
