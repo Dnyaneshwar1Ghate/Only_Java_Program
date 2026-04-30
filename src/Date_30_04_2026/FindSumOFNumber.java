de9f2fb82e8226eb76a868cccdd1f2d90bd1ac46
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-04-30 14:12:01 +0530

    new

---
+++ src/Date_30_04_2026/FindSumOFNumber.java
@@ -0,0 +1,19 @@
+package Date_30_04_2026;
+
+import java.util.Scanner;
+
+public class FindSumOFNumber {
+	public static void main(String[] args) {
+		
+		int num1;
+		int num2;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter tow numbers:");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		System.out.println("sum of this number is "+num1+num2);
+		
+		
+	}
+
+}
---
+++ src/Date_30_04_2026/WhileDemoPrimeNumber.java
@@ -0,0 +1,26 @@
+package Date_30_04_2026;
+
+public class WhileDemoPrimeNumber {
+
+	public static void main(String[] args) {
+
+		int num=7;
+		int count=0;
+		
+			for(int i=0;i<=num;i++)
+			{
+				if(num%i==0)
+				{
+					count++;
+				}
+			}
+			if(count ==2)
+			{
+				System.out.println("prime");
+			}else {
+				System.out.println("not prime ");
+			}
+			
+		
+	}
+}
