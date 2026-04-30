82e6dcfdd0f2e51c7a22932f05f20c15b7e8d389
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-04-30 11:14:59 +0530

    new

---
+++ src/Date_30_04_2026/FirstDemo.java
@@ -0,0 +1,25 @@
+package Date_30_04_2026;
+
+public class FirstDemo {
+
+	
+	
+	public static boolean evenNumber(int num)
+	{
+		
+		if(num%2==0)
+		{
+				return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+	
+		System.out.println(evenNumber(70));
+	}
+}
