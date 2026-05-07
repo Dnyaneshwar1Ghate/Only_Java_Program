ddba8c4f2ee0dbc3042ee4f03e3c0856894c89cb
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-05-07 11:54:54 +0530

    new

---
+++ src/Programs/WiproProgram.java
@@ -0,0 +1,21 @@
+package Programs;
+
+public class WiproProgram {
+
+	public static void main(String[] args) {
+
+		String str = "iloveabcbecauseiworkwithabcq";
+		String result = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (i <= str.length() - 3 && str.charAt(i) == 'a' && str.charAt(i + 1) == 'b' && str.charAt(i + 2) == 'c') {
+				
+				result=result+"Wipro";
+				i=i+2;
+				
+			}else {
+				result=result+str.charAt(i);
+			}
+		}
+		System.out.println(result);
+	}
+}
