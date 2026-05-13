a4cb17576be319693eef1753794dadfb44ae0345
Dnyaneshwar G. <dnyaneshwarghate1010@gmail.com>
2026-05-13 11:42:14 +0530

    new

---
+++ src/All_Top_Java_Programs/EvenOdd.java
@@ -0,0 +1,16 @@
+package All_Top_Java_Programs;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		
+		int num=30;
+		if(num%2==0)
+		{
+			System.out.println("Even");
+		}else
+		{
+			System.out.println("odd");
+		}
+	}
+}
